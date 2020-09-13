b87c07e02d8ca7ec7852f9d178028d2c3f1cd7dd
ayoub Er-razki <ayouberrazki@outlook.com>
2020-09-13 03:21:27 +0200

    setting up Api

---
+++ netcoreBack/Controllers/TarjetaCreditoesController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using netcoreBack;
+using netcoreBack.Models;
+
+namespace netcoreBack.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TarjetaCreditoesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TarjetaCreditoesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TarjetaCreditoes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TarjetaCredito>>> GettarjetaCredito()
+        {
+            return await _context.tarjetaCredito.ToListAsync();
+        }
+
+        // GET: api/TarjetaCreditoes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TarjetaCredito>> GetTarjetaCredito(int id)
+        {
+            var tarjetaCredito = await _context.tarjetaCredito.FindAsync(id);
+
+            if (tarjetaCredito == null)
+            {
+                return NotFound();
+            }
+
+            return tarjetaCredito;
+        }
+
+        // PUT: api/TarjetaCreditoes/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTarjetaCredito(int id, TarjetaCredito tarjetaCredito)
+        {
+            if (id != tarjetaCredito.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(tarjetaCredito).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TarjetaCreditoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/TarjetaCreditoes
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<TarjetaCredito>> PostTarjetaCredito(TarjetaCredito tarjetaCredito)
+        {
+            _context.tarjetaCredito.Add(tarjetaCredito);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTarjetaCredito", new { id = tarjetaCredito.Id }, tarjetaCredito);
+        }
+
+        // DELETE: api/TarjetaCreditoes/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<TarjetaCredito>> DeleteTarjetaCredito(int id)
+        {
+            var tarjetaCredito = await _context.tarjetaCredito.FindAsync(id);
+            if (tarjetaCredito == null)
+            {
+                return NotFound();
+            }
+
+            _context.tarjetaCredito.Remove(tarjetaCredito);
+            await _context.SaveChangesAsync();
+
+            return tarjetaCredito;
+        }
+
+        private bool TarjetaCreditoExists(int id)
+        {
+            return _context.tarjetaCredito.Any(e => e.Id == id);
+        }
+    }
+}
