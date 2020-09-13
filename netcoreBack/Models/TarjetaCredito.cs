33175eab6cd6605593682ddbe3759b12cb8fa949
ayoub Er-razki <ayouberrazki@outlook.com>
2020-09-13 02:13:11 +0200

    Adding Model

---
+++ netcoreBack/Models/TarjetaCredito.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace netcoreBack.Models
+{
+    public class TarjetaCredito
+    { 
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Column(TypeName="varchar(255)")]
+        public string Titular { get; set; }
+    
+        public string NumeroTarjeta { get; set; }
+
+
+
+
+
+    }
+}
