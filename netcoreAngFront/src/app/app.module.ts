d0cd12ca439ebdf43ae930c53551e72a4d640567
ayoub Er-razki <ayouberrazki@outlook.com>
2020-09-28 00:01:12 +0200

    setting up components Ang

---
+++ netcoreAngFront/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TarjetasComponent } from './components/tarjetas/tarjetas.component';
+import { TarjetasCreditoComponent } from './components/tarjetas/tarjetas-credito/tarjetas-credito.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TarjetasComponent,
+    TarjetasCreditoComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
